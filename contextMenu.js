d829335d2cebde3ac871d9031a739ef896b05311
Yuta Suzuki <banquet.of.merry.widow.y.s@gmail.com>
2013-05-08 00:43:11 +0900

    デモレベルでほぼ完成
    一段落。
    次は見た目？

--- contextMenu.js
+++ contextMenu.js
@@ -4,6 +4,32 @@ chrome.contextMenus.create({
 	"contexts": ["image"],
 	"onclick": function(info){
 		var url = 'http://visseeker2.yahoo-labs.jp/yipr/textsearch?results=8&start=1&mode=text&query=' + encodeURIComponent( info.srcUrl);
+
+		var xhr = new XMLHttpRequest();
+		//XMLを受け取った時の処理
+		xhr.onreadystatechange = function(){
+			localStorage.clear();
+			if( xhr.readyState == 4 && xhr.status == 200 ){
+				var urls = xhr.responseXML.getElementsByTagName("Url");
+				localStorage["itemnum"] = urls.length/2;
+				for( var i=0; i<urls.length; i+=2 ){
+					localStorage["url" + (i/2)] = urls[i].firstChild.nodeValue;
+				}
+
+				var titles = xhr.responseXML.getElementsByTagName("Title");
+				for( var i=0; i<titles.length; i++ ){
+					localStorage["title"+i] = titles[i].firstChild.nodeValue;
+				}
+
+				var clickurls = xhr.responseXML.getElementsByTagName("ClickUrl");
+				for( var i=0; i<clickurls.length; i++ ){
+					localStorage["clickurl"+i] = clickurls[i].firstChild.nodeValue;
+				}
+			}
+		}
+		xhr.open('GET', url, false);
+		xhr.send(null);
+
 		chrome.tabs.create({
 			url: chrome.extension.getURL("result.html")
 		});
--- visserchResult.js
+++ visserchResult.js
@@ -1,7 +1,19 @@
-//PageActionする？
+var entryPoint = document.getElementById("result");
+var itemnum = localStorage["itemnum"];
+for( var i=0; i<itemnum; i++ ){
+	var title = localStorage["title" + i];
+	var clickurl = localStorage["clickurl" + i];
+	var url = localStorage["url" + i];
 
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
-	if( tab.url.indexOf('visseeker2') != -1 ){
-		chrome.pageAction.show(tabId);
-	}
-});
+	var imgNode = document.createElement("img");
+	imgNode.src = url;
+	var titleNode = document.createElement("a");
+	titleNode.href = clickurl;
+	titleNode.target = "_blank";
+	titleNode.appendChild(document.createTextNode(title));
+	
+	entryPoint.appendChild(titleNode);
+	entryPoint.appendChild(document.createElement("br"));
+	entryPoint.appendChild(imgNode);
+	entryPoint.appendChild(document.createElement("br"));
+}
